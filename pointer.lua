be309f3d407a1270e4a5cb6a32e41270af5623c9
Mansour Moufid <mansourmoufid@gmail.com>
2019-09-20 21:24:15 -0400

    Refactor pointer packing.

--- pointer.lua
+++ pointer.lua
@@ -4,62 +4,38 @@ local pointer = {}
 
 local ffi = require('ffi')
 
-local function split(x, w, n, f, g)
+local function split(x)
+    local t = ffi.typeof(x)
+    local n = ffi.sizeof(t)
+    local two = ffi.new(t, 2)
     local xs = {}
-    local two = f(2)
     for i = 1, n do
-        local j = i - 1
-        local a = two ^ (w * (j + 1))
-        local b = two ^ (w * j)
+        local a = two ^ (8 * (i))
+        local b = two ^ (8 * (i - 1))
         if b == 0 then
             break
         end
         local z = (a > 0) and (x % a / b) or (x / b)
-        xs[i] = g(z)
+        xs[i] = tonumber(z)
     end
     return xs
 end
 
-local function join(xs, w, f)
-    local z = f(0)
-    local two = f(2)
+local function join(t, xs)
+    local two = ffi.new(t, 2)
+    local z = ffi.new(t, 0)
     for i, x in ipairs(xs) do
-        local j = i - 1
-        z = z + f(x) * two ^ (w * j)
+        z = z + x * two ^ (8 * (i - 1))
     end
     return z
 end
 
-local tointptr = function (x) return ffi.new('uintptr_t', x) end
-
 function pointer.pack(type, x)
-    return {
-        type = type,
-        address = split(
-            ffi.cast('uintptr_t', x),
-            16,
-            4,
-            tointptr,
-            tonumber
-        ),
-    }
+    return {type = type, address = split(ffi.cast('uintptr_t', x))}
 end
 
 function pointer.unpack(x)
-    return ffi.cast(
-        x.type .. ' *',
-        join(
-            x.address,
-            16,
-            tointptr
-        )
-    )
+    return ffi.cast(x.type .. ' *', join('uintptr_t', x.address))
 end
-
---local x = ffi.new('uint64_t', 31415926535)
---local f = function (x) return ffi.new('uint64_t', x) end
---local y = split(x, 8, 10, f, tonumber)
---local z = join(y, 8, f)
---assert(x == z)
 
 return pointer
